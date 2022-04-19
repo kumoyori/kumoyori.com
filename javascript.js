655d6197f6766a8c6298cb365fd76c3b56574b4f
Zekeby <zekeby@fastmail.com>
2022-04-19 22:55:30 +0300

    I made a button that puts a message on log.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
 function hello() {
-  alert("Hello!")
+  alert("Hello!");
+}
+
+function something() {
+  alert("Something!");
+}
+
+function test() {
+  alert("Look at the log.")
+  console.log("Congrats! You looked at the console.")
 }
