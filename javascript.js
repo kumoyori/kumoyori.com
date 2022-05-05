43ac1a72a5465083f91796420b6dac241a0355e9
Zekeby <zekeby@fastmail.com>
2022-05-05 19:55:22 +0300

    Added menus.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,45 @@
-function hello() {
-  alert("Hello!");
+const homePage = document.getElementById("home");
+const programsPage = document.getElementById("programs");
+const pageTitle = document.getElementById("title");
+
+homePage.style.display = "block";
+programsPage.style.display = "none";
+
+pageTitle.textContent = "<Main Page>"
+
+function home() {
+  pageTitle.textContent = "<Main Page>"
+
+  if (programsPage.style.display == "block") {
+    programsPage.style.display = "none";
+  }
+
+  if (homePage.style.display == "none") {
+    homePage.style.display = "block";
+  }
 }
 
-function something() {
-  alert("Something!");
+
+function programs() {
+  pageTitle.textContent = "<Programs>"
+
+  if (homePage.style.display == "block") {
+    homePage.style.display = "none";
+  }
+
+  if (programsPage.style.display == "none") {
+    programsPage.style.display = "block";
+  }
 }
 
-function test() {
-  alert("Look at the log.")
-  console.log("Congrats! You looked at the console.")
+/*
+function programs() {
+  if (homePage.style.display == "none") {
+    homePage.style.display = "block";
+  }
+
+  if (programsPage.style.display == "none") {
+    programsPage.style.display = "block";
+  }
 }
+*/
