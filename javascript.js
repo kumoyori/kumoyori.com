4f0db0650a6767cb2c1b234d19c25238bf7b302a
Zekeby <zekeby@fastmail.com>
2022-05-09 15:43:00 +0300

    Added iframe

--- javascript.js
+++ javascript.js
@@ -1,45 +1,18 @@
 const homePage = document.getElementById("home");
 const programsPage = document.getElementById("programs");
 const pageTitle = document.getElementById("title");
+const panelPage = document.getElementById("panel")
 
 homePage.style.display = "block";
 programsPage.style.display = "none";
 
-pageTitle.textContent = "<Main Page>"
+pageTitle.textContent = "<Main Page>";
 
-function home() {
-  pageTitle.textContent = "<Main Page>"
 
-  if (programsPage.style.display == "block") {
-    programsPage.style.display = "none";
-  }
-
-  if (homePage.style.display == "none") {
-    homePage.style.display = "block";
-  }
+function programsPanel() {
+    panelPage.src = "programs.html" 
 }
 
-
-function programs() {
-  pageTitle.textContent = "<Programs>"
-
-  if (homePage.style.display == "block") {
-    homePage.style.display = "none";
-  }
-
-  if (programsPage.style.display == "none") {
-    programsPage.style.display = "block";
-  }
+function homePanel() {
+    panelPage.src = "home.html"
 }
-
-/*
-function programs() {
-  if (homePage.style.display == "none") {
-    homePage.style.display = "block";
-  }
-
-  if (programsPage.style.display == "none") {
-    programsPage.style.display = "block";
-  }
-}
-*/
